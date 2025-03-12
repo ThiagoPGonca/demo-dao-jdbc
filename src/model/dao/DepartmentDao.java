2009083aa023485ca556403417548acc53af8c70
Thiago <thiagopersiadm@gmail.com>
2025-03-12 16:20:50 -0300

    DepartmentDao e SellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    public void insert(Department obj);
+    public void update(Department obj);
+    public void deleteById(Integer id);
+    public Department findById(Integer id);
+    public List<Department> findAll();
+
+}
