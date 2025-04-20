cb7b5ce3cc14a3db2d26a1abf54e8898f56273ca
Thiago <thiagopersiadm@gmail.com>
2025-04-20 19:05:27 -0300

    Update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -35,5 +35,11 @@ public class Main {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+
+        System.out.println("\n=== Tetes 5: Seller Update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
     }
 }
